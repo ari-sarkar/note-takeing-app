8ebf51d80140d65106a8301bb012ef37a2d29e18
ari-sarkar <arisarkar1995@gmail.com>
2020-12-30 12:22:10 +0530

    fixed some issuses now the filter part is due

--- src/Components/UI/Notebook.js
+++ src/Components/UI/Notebook.js
@@ -1,44 +1,62 @@
 import React, { useState } from "react";
 import "../../Styles/UI/Notebook.scss";
-import FilterLogic from "../UILogic/FilterLogic";
-import EachNote from "../UILogic/EachNote"
+import EachNote from "../UILogic/EachNote";
 const Notebook = () => {
-    const [ title, settitle ] = useState(null)
-    const [ note, setnote ] = useState(null)
-    const [ isClicked, setisClicked ] = useState(false)
-    const AddNote= (e) => {
-        e.preventDefault()
-        setisClicked(true)
-    }
+  const [title, settitle] = useState(null);
+  const [note, setnote] = useState(null);
+  const [isClicked, setisClicked] = useState(false);
+  const AddNote = e => {
+    e.preventDefault();
+    setisClicked(true);
+  };
+  //Filter Logic
+  let [month, date, year] = new Date().toLocaleDateString("en-US").split("/");
+  let [hour, minute] = new Date().toLocaleTimeString("en-US").split(/:| /);
+  //console.log(month, date, year, hour, minute, second);
   return (
     <section>
-        <h1>NoteBook</h1>
+      <h1>NoteBook</h1>
       <div className="notebook-container">
         <div className="left-side-wrapper">
           <div className="write-notes">
             <form>
               <label>Title</label>
-              <input type="text" value={title} onChange={e => settitle(e.target.value)}></input>
+              <input
+                type="text"
+                value={title}
+                onChange={e => settitle(e.target.value)}
+              ></input>
               <br />
               <label>Write Notes</label>
               <br />
-              <textarea type="text" onChange={e => setnote(e.target.value)} value={note} ></textarea>
-              <button type="submit" onClick={AddNote}>Add Note</button>
+              <textarea
+                type="text"
+                onChange={e => setnote(e.target.value)}
+                value={note}
+              ></textarea>
+              <button type="submit" onClick={AddNote}>
+                Add Note
+              </button>
             </form>
           </div>
         </div>
         <div className="right-side-wrapper">
-          <div className="filter-notes">
-            <h5>Filter By</h5>
-            <div className="filter-logic-container">
-                <FilterLogic />
-            </div>
-          </div>
           <div className="display-notes">
-              <h2>Notes</h2>
-              <div className="display-notes-logic-container">
-                <EachNote title={isClicked ? title: ""} note={isClicked ? note: ""} setisClicked={setisClicked}/>
-              </div>
+            <h2>Notes</h2>
+            <div className="display-notes-logic-container">
+              <EachNote
+                title={isClicked ? title : ""}
+                note={isClicked ? note : ""}
+                setisClicked={setisClicked}
+                month={month}
+                date={date}
+                year={year}
+                hour={hour}
+                minute={minute}
+                settitle={settitle}
+                setnote={setnote}
+              />
+            </div>
           </div>
         </div>
       </div>
--- src/Components/UILogic/EachNote.js
+++ src/Components/UILogic/EachNote.js
@@ -1,38 +1,65 @@
-import React, { useEffect } from 'react'
-import "../../Styles/UILogic/EachNote.scss"
-const EachNote = ({ title, note, setisClicked }) => { 
-useEffect(() => {
-    if(note){
-        const notes = document.querySelector(".note-list")
-    const list = document.createElement("li")
-    //list.classList.add = "my-list"
-    const buttonDelete = document.createElement("button")
-    //buttonDelete.classList.add = "button-delete"
-    // const buttonEdit = document.createElement("button")
-    list.innerHTML = `<div><input value="${title}-${note}"></input></div>`
-    buttonDelete.innerHTML= `<div>
-    <button>Del</button>
-    </div>`
-    // buttonEdit.innerHTML= `<div>
-    // <button>Edit</button>
-    // </div>`
-    list.append(buttonDelete)
-        notes.appendChild(list) 
-        setisClicked(false)
-        buttonDelete.addEventListener("click", () => {
-            list.style.display = "none";
-        })
-        // buttonEdit.addEventListener("click", () => {
-        //     list.style.borderColor = "green";
-        // })
+import React, { useEffect } from "react";
+import "../../Styles/UILogic/EachNote.scss";
+import FilterLogic from "../UILogic/FilterLogic";
+const EachNote = ({
+  title,
+  note,
+  settitle,
+  setnote,
+  setisClicked,
+  date,
+  month,
+  year,
+  hour,
+  minute,
+}) => {
+  console.log(date, month, year, hour, minute);
+  useEffect(() => {
+    if (note) {
+      setisClicked(false);
+      ///logic
+      const notes = document.querySelector(".note-list");
+      const lists = document.createElement("li");
+      lists.classList.add("note");
+      lists.innerHTML = `<div contentEditable="true"><b>${title}:</b> ${note}</div>`;
+      const buttonDelete = document.createElement("button");
+      buttonDelete.classList.add("delete-button");
+      buttonDelete.innerText = "Del";
+      buttonDelete.addEventListener("click", () => {
+        //lists.innerHTML = "none";
+        lists.remove();
+      });
+      lists.appendChild(buttonDelete);
+      notes.appendChild(lists);
+      notes.value = "";
+      console.log(notes.childNodes);
+      settitle("");
+      setnote("");
     }
-},[setisClicked, note, title])
-        
-        
-    return (
-        <div className="note-list">
-        </div>
-    )
-}
+  }, [
+    setisClicked,
+    note,
+    title,
+    date,
+    month,
+    year,
+    hour,
+    minute,
+    setnote,
+    settitle,
+  ]);
 
-export default EachNote
+  return (
+    <div className="filter-eachnote-container">
+      <div className="filter-notes">
+        <h5>Filter By</h5>
+        <div className="filter-logic-container">
+          <FilterLogic />
+        </div>
+      </div>
+      <ul className="note-list"></ul>
+    </div>
+  );
+};
+
+export default EachNote;
